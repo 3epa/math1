d4155786c1cf8832d9078428d0b0834c99e5ba90
3epa <egorfelix@yandex.ru>
2025-03-04 01:31:47 +0300

    add file reading realisation

---
+++ src/main/java/ru/itmo/command/FileCommand.java
@@ -0,0 +1,68 @@
+package ru.itmo.command;
+
+import ru.itmo.algo.GaussMethod;
+import ru.itmo.exception.IncorrectInputException;
+import ru.itmo.model.Matrix;
+
+import java.io.*;
+
+public class FileCommand implements Command {
+    @Override
+    public String getName() {
+        return "file";
+    }
+
+    @Override
+    public void execute() {
+        Matrix matrix = null;
+        try {
+            System.out.println("Введите имя файла: ");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String fileName = reader.readLine().strip();
+
+            System.out.println("Чтение данных с файла...");
+            matrix = readFromFile(fileName);
+        } catch (FileNotFoundException e) {
+            System.out.println("Указанный файл не существует");
+            return;
+        } catch (IOException e) {
+            System.out.println("Внутренняя ошибка, попробуйте перезапустить приложение или проверить данные файла");
+            return;
+        } catch (IncorrectInputException e) {
+            System.out.println("Данные не соответствуют требуемым ограничениям");
+            System.out.println(e.getMessage());
+            return;
+        } catch (NumberFormatException e) {
+            System.out.println("Одно из введенных значений не является числом");
+            return;
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Некорректное количество введенных данных");
+            return;
+        }
+        GaussMethod.compute(matrix);
+    }
+
+    private Matrix readFromFile(String fileName) throws IOException, IncorrectInputException {
+        BufferedReader reader = new BufferedReader(new FileReader(fileName));
+
+        int size = Integer.parseInt(reader.readLine().strip());
+        if (size > 20 || size < 1) {
+            throw new IncorrectInputException("Variable size not in required range(1 <= size <= 20)");
+        }
+        String[][] stringMatrix = new String[size][size + 1];
+        for (int i = 0; i < size; i++) {
+            String matrixLine = reader.readLine().strip();
+            matrixLine = matrixLine.replaceAll(",", ".");
+            stringMatrix[i] = matrixLine.split(" ");
+        }
+
+        double[][] matrix = new double[size][size + 1];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size + 1; j++) {
+                matrix[i][j] = Double.parseDouble(stringMatrix[i][j].strip());
+            }
+        }
+        reader.close();
+        return new Matrix(size, matrix);
+    }
+}
