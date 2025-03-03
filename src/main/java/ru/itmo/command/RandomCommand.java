aaf8ae58c1f989ece4d39c32a4e7f4b284ad3f1f
3epa <egorfelix@yandex.ru>
2025-03-04 01:44:32 +0300

    add random matrix generation realisation

---
+++ src/main/java/ru/itmo/command/RandomCommand.java
@@ -0,0 +1,67 @@
+package ru.itmo.command;
+
+import ru.itmo.algo.GaussMethod;
+import ru.itmo.exception.IncorrectInputException;
+import ru.itmo.model.Matrix;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class RandomCommand implements Command {
+    @Override
+    public String getName() {
+        return "random";
+    }
+
+    @Override
+    public void execute() {
+        Matrix matrix = null;
+        System.out.println("Генерация рандомной матрицы...");
+        try {
+            matrix = generateRandomMatrix();
+        } catch (IOException e) {
+            System.out.println("Внутренняя ошибка, попробуйте перезапустить приложение или проверить данные файла");
+            return;
+        } catch (IncorrectInputException e) {
+            System.out.println("Данные не соответствуют требуемым ограничениям");
+            System.out.println(e.getMessage());
+            return;
+        } catch (NumberFormatException e) {
+            System.out.println("Одно из введенных значений не является числом");
+            return;
+        }
+        printMatrix(matrix);
+        GaussMethod.compute(matrix);
+    }
+
+    private Matrix generateRandomMatrix() throws IncorrectInputException, IOException {
+        System.out.println("Введите размерность матрицы:");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int size = Integer.parseInt(reader.readLine().strip());
+        if (size > 20 || size < 1) {
+            throw new IncorrectInputException("Variable size not in required range(1 <= size <= 20)");
+        }
+        double[][] matrix = new double[size][size + 1];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size + 1; j++) {
+                matrix[i][j] = Math.random()*100-50;
+            }
+        }
+        reader.close();
+        return new Matrix(size, matrix);
+    }
+
+    private void printMatrix(Matrix matrix) {
+        int size = matrix.getSize();
+        double[][] data = matrix.getData();
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size + 1; j++) {
+                System.out.print(data[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
